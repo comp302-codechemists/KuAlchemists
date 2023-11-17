8c35d77e73a8ba13143b27d0e1532f153753adbe
simgeakgul <simgeakgul20@ku.edu.tr>
2023-11-18 00:58:03 +0300

    First code.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Create and display the frame
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new WelcomeFrame().setVisible(true);
+            }
+        });
+    }
+
+}
---
+++ src/WelcomeFrame.java
@@ -0,0 +1,80 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+
+public class WelcomeFrame extends JFrame {
+
+    private JButton startButton;
+    private JLabel loadingLabel;
+    private JProgressBar progressBar;
+
+    public WelcomeFrame() {
+        setTitle("Welcome to the Game");
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+        
+        
+        JPanel backgroundPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+           
+                ImageIcon imageIcon = new ImageIcon("C:\\Users\\simge\\git\\KuAlchemists\\src\\bck.png");
+                Image image = imageIcon.getImage();
+
+               
+                int x = (getWidth() - image.getWidth(null)) / 2;
+                int y = (getHeight() - image.getHeight(null)) / 2;
+                g.drawImage(image, x, y, this);
+            }
+        };
+        backgroundPanel.setLocation(0, 0);
+        backgroundPanel.setSize(new Dimension(586, 363));
+        backgroundPanel.setLayout(null);
+        getContentPane().add(backgroundPanel);
+        
+        startButton = new JButton("Start Game");
+        startButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        startButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        startButton.setBackground(new Color(0, 150, 136)); 
+        startButton.setForeground(Color.WHITE); 
+        startButton.setFont(new Font("Arial", Font.BOLD, 12)); 
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startButtonClicked();
+            }
+        });
+        
+
+        loadingLabel = new JLabel("   Codechemists is loading, this may take a while...");
+        loadingLabel.setForeground(Color.WHITE);
+        loadingLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 13));
+        loadingLabel.setBounds(127, 199, 354, 30);
+        loadingLabel.setVisible(false);
+        startButton.setBounds(248, 200, 100, 30); 
+        loadingLabel.setBackground(Color.GRAY);
+        loadingLabel.setOpaque(true);
+        backgroundPanel.add(loadingLabel);
+        backgroundPanel.add(startButton);
+        backgroundPanel.setOpaque(false);
+        
+        progressBar = new JProgressBar();
+        progressBar.setBounds(229, 240, 160, 11);
+        progressBar.setValue(20);
+        progressBar.setVisible(false);
+        backgroundPanel.add(progressBar);
+        
+    }
+
+    private void startButtonClicked() {
+        startButton.setVisible(false);
+        loadingLabel.setVisible(true);
+        progressBar.setVisible(true);
+
+    }
+}
