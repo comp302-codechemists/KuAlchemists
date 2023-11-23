d76cd1d91b123d61b15293e42abbc667404143b9
simgeakgul <simgeakgul20@ku.edu.tr>
2023-11-23 12:04:14 +0300

    first simge commit.

--- src/WelcomeFrame.java
+++ src/WelcomeFrame.java
@@ -22,7 +22,7 @@ public class WelcomeFrame extends JFrame {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
            
-                ImageIcon imageIcon = new ImageIcon("/Users/beyzaerdogan/Desktop/COMP302/KuAlchemists/src/bck.png");
+                ImageIcon imageIcon = new ImageIcon("C:\\KuAlchemists\\KuAlchemists\\src\\bck.png");
                 Image image = imageIcon.getImage();
 
                
