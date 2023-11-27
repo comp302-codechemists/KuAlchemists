f35aca59edffce457491ddee85c1305f2bf3229c
mberkyavas <myavas@ku.edu.tr>
2023-11-27 14:58:45 +0300

    berk deneme

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
             @Override
             public void run() {
                 new WelcomeFrame().setVisible(true);
-                System.out.println("SONDENEME");
+                System.out.println("berk deneme");
             }
         });
     }
