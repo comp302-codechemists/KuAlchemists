8ffaf1617570a7e69a92d075503e7c741f38607d
mberkyavas <myavas@ku.edu.tr>
2023-11-28 16:29:50 +0300

    artifacts

---
+++ src/Business/PotionEffectBoosterArtifact.java
@@ -0,0 +1,37 @@
+package Business;
+
+public class PotionEffectBoosterArtifact extends Artifact {
+    private int boostAmount;
+    public String whereToApply;
+    private String duration;
+
+    public PotionEffectBoosterArtifact(int boostAmount, String whereToApply, String duration) {
+        this.boostAmount = boostAmount;
+        this.whereToApply = whereToApply;
+        this.duration = duration;
+    }
+
+    public int getBoostAmount() {
+        return boostAmount;
+    }
+
+    public void setBoostAmount(int boostAmount) {
+        this.boostAmount = boostAmount;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+    
+    public String getWhereToApply() {
+        return whereToApply;
+    }
+
+    public void setWhereToApply(String whereToApply) {
+        this.whereToApply = whereToApply;
+    }
+}
