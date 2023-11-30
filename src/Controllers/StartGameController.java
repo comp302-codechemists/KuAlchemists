ff9313cc6964d85208c9780d3ecf685f17332937
simgeakgul <simgeakgul20@ku.edu.tr>
2023-11-30 14:28:54 +0300

    changed controller

--- src/Controllers/StartGameController.java
+++ src/Controllers/StartGameController.java
@@ -6,6 +6,8 @@ public class StartGameController {
 	
 	public void handleStartGame(String p1name, String p2name, String avatar1, String avatar2) {
 		
+		
+		//TODO turn names and avatars into a list
     	KUAlchemistsGame game = new KUAlchemistsGame(2);
     	game.play(p1name, p2name, avatar1, avatar2);
 	}
