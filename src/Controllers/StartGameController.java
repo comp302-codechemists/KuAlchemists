c50f0742ecad86029623fbf6d084614a2fde07a9
Bkurtuldu <bkurtuldu20@ku.edu.tr>
2023-11-30 13:56:56 +0300

    StartGameController behaviour changed

--- src/Controllers/StartGameController.java
+++ src/Controllers/StartGameController.java
@@ -1,13 +1,23 @@
 package Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import Business.KUAlchemistsGame;
 
 public class StartGameController {
 	
 	public void handleStartGame(String p1name, String p2name, String avatar1, String avatar2) {
 		
+		List<String> nameList = new ArrayList<String>();
+		nameList.add(p1name);
+		nameList.add(p2name);
+		List<String> avatarList = new ArrayList<String>();
+		avatarList.add(avatar1);
+		avatarList.add(avatar2);
+		
     	KUAlchemistsGame game = new KUAlchemistsGame(2);
-    	game.play(p1name, p2name, avatar1, avatar2);
+    	game.play(nameList,avatarList);
 	}
 
 }
