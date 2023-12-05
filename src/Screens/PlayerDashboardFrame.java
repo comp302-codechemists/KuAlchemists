57238f6ec60ecba7e974a8c80d87c79cdb0268ae
simgeakgul <simgeakgul20@ku.edu.tr>
2023-12-05 10:55:41 +0300

    dahboard added buttons

--- src/Screens/PlayerDashboardFrame.java
+++ src/Screens/PlayerDashboardFrame.java
@@ -3,6 +3,8 @@ package Screens;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
@@ -19,6 +21,7 @@ public class PlayerDashboardFrame extends GeneralFrame{
 	 * submitting publications.
 	 */
 	private JPanel backgroundPanel;
+	private JPanel bottom;
 	
 	public PlayerDashboardFrame(Player player) {
 		
@@ -48,51 +51,73 @@ public class PlayerDashboardFrame extends GeneralFrame{
             }
     	};
     	
-        backgroundPanel.setLocation(0, 0);
+        backgroundPanel.setLocation(10, 0);
         backgroundPanel.setLayout(null);
         backgroundPanel.setOpaque(false);
-        backgroundPanel.setSize(new Dimension(964, 552));
-        
-        // add every component to background panel
-
+        backgroundPanel.setSize(new Dimension(789, 466));       
         getContentPane().add(backgroundPanel);
         
         
-        // How to do this? We should know which button is clicked.
-        // But implementing every 28 button one by one doesn't sound good.
-        /*
+        
+        // Buttons for the deduction board:
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setBounds(10, 10, 944, 532);
-        backgroundPanel.add(buttonPanel);
+        buttonPanel.setOpaque(false);
+        buttonPanel.setLayout(null);
+        buttonPanel.setBounds(10, 10, 769, 456);
+        
         ButtonGroup buttonGroup = new ButtonGroup();
-        int pyramidHeight = 7; 
-        int buttonWidth = 100;
-        int buttonHeight = 110;
-        int startX = 150;
-        int startY = 50;
-        for (int i = 0; i < pyramidHeight; i++) {
-            int buttonsInRow = i + 1;
-            int rowWidth = buttonsInRow * buttonWidth;
+        List<JRadioButton> buttons = new ArrayList<JRadioButton>();
 
-            for (int j = 0; j < buttonsInRow; j++) {
-                int x = startX + (rowWidth - (buttonsInRow * buttonWidth)) / 2 + j * buttonWidth;
-                int y = startY + i * buttonHeight;
+        int rows = 7;
+        int startX = 372;
+        int startY = 20; 
 
-                JRadioButton radioButton = new JRadioButton();
-                radioButton.setBounds(x, y, buttonWidth, buttonHeight);
-                buttonGroup.add(radioButton);
-                buttonPanel.add(radioButton);
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col <= row; col++) {
+                JRadioButton btn = new JRadioButton();
+                buttonGroup.add(btn);
+                buttons.add(btn);
+                int x = startX - row * 65 / 2 + col * 65;
+                int y = startY + row * 57;
+                btn.setBounds(x, y, 20, 20);
+                buttonPanel.add(btn);
             }
+        }   
+        
+        backgroundPanel.add(buttonPanel);
+    
+    
+    bottom = new JPanel() {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            Image originalImage = new ImageIcon(this.getClass().getResource("/Images/deduction_board_bottom.png")).getImage();
+
+            // Calculate the scaled width and height while maintaining the aspect ratio
+            int originalWidth = originalImage.getWidth(null);
+            int originalHeight = originalImage.getHeight(null);
+            int maxWidth = getWidth();
+            int maxHeight = getHeight();
+
+            double scale = Math.min((double) maxWidth / originalWidth, (double) maxHeight / originalHeight);
+            int scaledWidth = (int) (originalWidth * scale);
+            int scaledHeight = (int) (originalHeight * scale);
+
+            // Calculate the position to center the scaled image
+            int x = (getWidth() - scaledWidth) / 2;
+            int y = (getHeight() - scaledHeight) / 2;
+
+            // Draw the scaled image
+            g.drawImage(originalImage, x, y, scaledWidth, scaledHeight, this);
         }
-        */
-        
-        
-        
-        
-		
+    };
+    bottom.setBounds(10, 476, 789, 297);
+    getContentPane().add(bottom);
+    bottom.setLayout(null);
+    bottom.setOpaque(false);
+	
 	}
-	
-	
 	
     public static void main(String[] args) {
         // Create and display the frame   
